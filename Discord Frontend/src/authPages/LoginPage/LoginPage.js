68bead758400746727983b602809cec90161ffeb
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2024-09-23 19:50:27 +0530

    Add files via upload

---
+++ Discord Frontend/src/authPages/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AuthBox from '../../shared/components/AuthBox';
+const LoginPage = () => {
+    return (
+        <AuthBox>
+            Login
+        </AuthBox>
+    );
+};
+
+export default LoginPage;
---
+++ Discord Frontend/src/authPages/LoginPage/LoginPageInputs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthBox from "../../shared/components/AuthBox";
+import LoginPageHeader from "./LoginPageHeader";
+import LoginPageInputs from "./LoginPageInputs";
+
+const LoginPage = () => {
+    return {
+        <AuthBox>
+    }
+}
