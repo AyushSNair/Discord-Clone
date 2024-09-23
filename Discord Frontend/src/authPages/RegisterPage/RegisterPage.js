77b678875b0412b5dc6224cb7402b8abecad0e48
Ayush Nair <64899298+AyushSNair@users.noreply.github.com>
2024-09-23 19:51:18 +0530

    Add files via upload

---
+++ Discord Frontend/src/authPages/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AuthBox from '../../shared/components/AuthBox';
+const RegisterPage = () => {
+    return (
+        <AuthBox>
+            Register Page
+        </AuthBox>
+    );
+};
+
+export default RegisterPage;
